df9cb064c9d519ff1fff9b163032e1b52e5d4fea
Iury Silva de Almeida <iury.colombo@gmail.com>
2025-01-21 12:16:35 -0300

    Adicionando a entidade de games e a conexao com o banco de dados h2

---
+++ src/main/java/br/com/iuryalmeida/Project_PixelShelf_Backend/entities/Game.java
@@ -0,0 +1,99 @@
+package br.com.iuryalmeida.Project_PixelShelf_Backend.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_game")
+public class Game implements Comparable<Game> {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+
+    @Column(name = "game_year")
+    private Integer year;
+    private String platform;
+    private String imgUrl;
+    private String description;
+    private String longDescription;
+
+    public Game() {
+    }
+
+    public Game(Long id, String title, Integer year, String platform, String imgUrl, String description, String longDescription) {
+        this.id = id;
+        this.title = title;
+        this.year = year;
+        this.platform = platform;
+        this.imgUrl = imgUrl;
+        this.description = description;
+        this.longDescription = longDescription;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public String getPlatform() {
+        return platform;
+    }
+
+    public void setPlatform(String platform) {
+        this.platform = platform;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLongDescription() {
+        return longDescription;
+    }
+
+    public void setLongDescription(String longDescription) {
+        this.longDescription = longDescription;
+    }
+
+    @Override
+    public int compareTo(Game other) {
+        return this.title.compareTo(other.title);
+    }
+}
