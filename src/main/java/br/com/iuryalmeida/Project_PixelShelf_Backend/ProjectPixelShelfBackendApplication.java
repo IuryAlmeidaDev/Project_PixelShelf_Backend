0ff51d6becb61134d0faa9f53c892ff0dd07d657
Iury Silva de Almeida <iury.colombo@gmail.com>
2025-01-21 11:44:24 -0300

    Subindo primeira parte do projeto

---
+++ src/main/java/br/com/iuryalmeida/Project_PixelShelf_Backend/ProjectPixelShelfBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.iuryalmeida.Project_PixelShelf_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectPixelShelfBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectPixelShelfBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/iuryalmeida/Project_PixelShelf_Backend/ProjectPixelShelfBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.iuryalmeida.Project_PixelShelf_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectPixelShelfBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
